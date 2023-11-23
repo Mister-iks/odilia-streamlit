c3b1da5f3a237d9b3dd5b959191663c85d662b0b
Hokage 8e <ibrahimapro289@gmail.com>
2023-11-23 09:30:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,12 @@ import numpy as np
 import pickle
 import streamlit as st
 
-st.write(f"scikit-learn version: {scikit-learn.__version__}")
-# Charger le modèle
 model = pickle.load(open('./modele.pkl', 'rb'))
 
-# Page d'accueil
 def home():
     st.title("Votre Application Streamlit")
-    # Utilisez st.write pour afficher des textes, des instructions, etc.
     st.write("Remplissez le formulaire ci-dessous pour obtenir une prédiction.")
 
-    # Interface utilisateur pour la saisie des valeurs
     checking_status = st.text_input("Statut du compte", "")
     duration = st.text_input("Durée du crédit", "")
     credit_history = st.text_input("Antécédents de crédit", "")
@@ -33,9 +28,7 @@ def home():
     sex = st.text_input("Genre", "")
     marriage = st.text_input("Statut matrimonial", "")
 
-    # Utilisez st.button pour le bouton de prédiction
     if st.button("Prédire"):
-        # Faire la prédiction
         input_features = np.array([float(checking_status), float(duration), float(credit_history), float(purpose),
                                    float(credit_amount), float(savings_status), float(employment),
                                    float(installment_commitment), float(other_parties), float(residence_since),
@@ -43,8 +36,6 @@ def home():
                                    float(num_dependents), float(own_telephone), float(foreign_worker), float(sex),
                                    float(marriage)]).reshape(1, -1)
         prediction = model.predict(input_features)
-
-        # Afficher le résultat
         st.success(f"Ce client est-il à risque ? {round(prediction[0], 2)}")
 
 if __name__ == "__main__":

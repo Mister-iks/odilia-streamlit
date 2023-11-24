76cebd3e2b448c12064cbafb550d3cac21ecbe43
Hokage 8e <ibrahimapro289@gmail.com>
2023-11-24 22:52:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ model = pickle.load(open('./modele.pkl', 'rb'))
 
 def home():
     st.title("Odilia SOME: Simulation de la solvabilité du client")
-    st.write("Remplissez le formulaire ci-dessous pour obtenir une prédiction.")
     image_url = "https://img.freepik.com/photos-gratuite/prise-vue-faible-angle-immeubles-grande-hauteur-sous-ciel-clair-francfort-allemagne_181624-30909.jpg"
+    st.write("Remplissez le formulaire ci-dessous pour obtenir une prédiction.")
     st.image(image_url, caption="Your Image Caption", use_column_width=True)
     checking_status = st.selectbox("Statut du compte", [0,1,2,3])
     duration = st.text_input("Durée du crédit", "")
